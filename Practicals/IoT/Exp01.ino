ab797ac0ead3d053c7131dea065339c3c948eef6
RAJJIT <2.0lairaj@gmail.com>
2025-04-09 23:25:38 +0530

    updated exp01

--- Practicals/IoT/Exp01.ino
+++ Practicals/IoT/Exp01.ino
@@ -1,75 +1,12 @@
 void setup()
 {
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop()
 {
-  // default
-  // digitalWrite(4, HIGH);
-  // delay(1);
-  // digitalWrite(4, LOW);
-  // delay(1);
-
-  // on
-  // off
-  // on on - delay
-  // off
-  // on on on  - more delay
-  // off
-
-  // digitalWrite(4, HIGH);
-  // delay(1000);
-  // digitalWrite(4, LOW);
-  // delay(1000);
-
-  // digitalWrite(4, HIGH);
-  // delay(1000);
-  // digitalWrite(4, LOW);
-  // delay(3000);
-
-  // digitalWrite(4, HIGH);
-  // delay(1000);
-  // digitalWrite(4, LOW);
-  // delay(5000);
-
-  digitalWrite(1, HIGH);
-  delay(1000);
-  digitalWrite(1, LOW);
-  delay(1000);
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-  delay(1000);
-  digitalWrite(3, HIGH);
-  delay(1000);
-  digitalWrite(3, LOW);
-  delay(1000);
-  digitalWrite(4, HIGH);
-  delay(1000);
-  digitalWrite(4, LOW);
-  delay(1000);
-  digitalWrite(5, HIGH);
-  delay(1000);
-  digitalWrite(5, LOW);
-  delay(1000);
-  digitalWrite(1, HIGH);
-  delay(1000);
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(3, HIGH);
-  delay(1000);
-  digitalWrite(4, HIGH);
-  delay(1000);
-  digitalWrite(5, HIGH);
-  delay(1000);
-
-  digitalWrite(5, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(2, LOW);
+  digitalWrite(12, HIGH);
+  delay(2000);
+  digitalWrite(12, LOW);
+  delay(2000);
 }
