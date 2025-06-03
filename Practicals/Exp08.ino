a452356762f6493787c740b16cfac77c0232e516
RAJJIT <2.0lairaj@gmail.com>
2025-06-03 21:07:55 +0530

    exp08 as prev commit was wrong

---
+++ Practicals/Exp08.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+SoftwareSerial bluetoothSerial(2, 3);
+const int relayPin = 7;
+void setup()
+{
+    pinMode(relayPin, OUTPUT);
+    Serial.begin(9600);
+    bluetoothSerial.begin(9600);
+}
+void loop()
+{
+    if (bluetoothSerial.available() > 0)
+    {
+        char command = bluetoothSerial.read();
+        if (command == '1')
+        {
+            digitalWrite(relayPin, HIGH);
+            Serial.println("Relay turned ON");
+        }
+        else if (command == '0')
+        {
+            digitalWrite(relayPin, LOW);
+            Serial.println("Relay turned OFF");
+        }
+    }
+}
