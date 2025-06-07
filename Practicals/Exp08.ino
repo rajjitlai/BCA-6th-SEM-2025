f9e01b37facac719d0b9aee2002f834b34512edc
RAJJIT <2.0lairaj@gmail.com>
2025-06-07 13:50:27 +0530

    updated exp 8 again

--- Practicals/Exp08.ino
+++ Practicals/Exp08.ino
@@ -1,32 +1,46 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial bluetoothSerial(2, 3); 
-const int relayPin = 7;
+char incomingData;
+int relayPin = 8;
+
+// RX = Arduino D2, TX = Arduino D3
+SoftwareSerial bluetoothSerial(2, 3); // RX, TX
 
 void setup()
 {
+    Serial.begin(9600);          // Serial Monitor
+    bluetoothSerial.begin(9600); // Bluetooth
     pinMode(relayPin, OUTPUT);
-    digitalWrite(relayPin, LOW);
-
-    Serial.begin(9600);         
-    bluetoothSerial.begin(9600);
+    digitalWrite(relayPin, LOW); // Relay OFF by default
 }
 
 void loop()
 {
-    if (bluetoothSerial.available() > 0)
+    // Check for Serial Monitor input
+    if (Serial.available())
     {
-        char command = bluetoothSerial.read();
+        incomingData = Serial.read();
+        handleCommand(incomingData);
+    }
 
-        if (command == '1')
-        {
-            digitalWrite(relayPin, HIGH);
-            Serial.println("Relay turned ON");
-        }
-        else if (command == '0')
-        {
-            digitalWrite(relayPin, LOW);
-            Serial.println("Relay turned OFF");
-        }
+    // Check for Bluetooth input
+    if (bluetoothSerial.available())
+    {
+        incomingData = bluetoothSerial.read();
+        handleCommand(incomingData);
+    }
+}
+
+void handleCommand(char cmd)
+{
+    if (cmd == '1')
+    {
+        digitalWrite(relayPin, HIGH); // Turn ON relay
+        Serial.println("Relay ON");
+    }
+    else if (cmd == '0')
+    {
+        digitalWrite(relayPin, LOW); // Turn OFF relay
+        Serial.println("Relay OFF");
     }
 }
