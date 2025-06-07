0af6f97a5eb800ee686add2c999995943a66c5d0
RAJJIT <2.0lairaj@gmail.com>
2025-06-07 13:18:20 +0530

    updated exp 7 without led

--- Practicals/Exp07.ino
+++ Practicals/Exp07.ino
@@ -3,17 +3,12 @@
 #include <SoftwareSerial.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-SoftwareSerial bluetoothSerial(2, 3);
-const int ledPin = 13;
+SoftwareSerial bluetoothSerial(0, 1); 
 
 void setup()
 {
     lcd.begin(16, 2);
     lcd.backlight();
-
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW);
-
     Serial.begin(9600);
     bluetoothSerial.begin(9600);
 }
@@ -22,26 +17,17 @@ void loop()
 {
     if (bluetoothSerial.available())
     {
-        String received = bluetoothSerial.readStringUntil('\n');
-        received.trim();
-        received.toUpperCase();
+        String received = bluetoothSerial.readStringUntil('\n'); 
+        received.trim();                                         
 
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Received:");
+
         lcd.setCursor(0, 1);
         lcd.print(received);
 
         Serial.print("Received Data: ");
         Serial.println(received);
-
-        if (received == "LEDON")
-        {
-            digitalWrite(ledPin, HIGH);
-        }
-        else if (received == "LEDOFF")
-        {
-            digitalWrite(ledPin, LOW);
-        }
     }
 }
