5cd5cbd9f7c319cbb9a28febbea2ebd8ba5ead15
RAJJIT <2.0lairaj@gmail.com>
2025-06-07 13:02:40 +0530

    updated exp 7

--- Practicals/Exp07.ino
+++ Practicals/Exp07.ino
@@ -1,47 +1,47 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 SoftwareSerial bluetoothSerial(2, 3);
+const int ledPin = 13;
+
 void setup()
 {
     lcd.begin(16, 2);
     lcd.backlight();
+
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW);
+
     Serial.begin(9600);
     bluetoothSerial.begin(9600);
 }
+
 void loop()
 {
-    if (bluetoothSerial.available() > 0)
+    if (bluetoothSerial.available())
     {
-        char data = bluetoothSerial.read();
+        String received = bluetoothSerial.readStringUntil('\n');
+        received.trim();
+        received.toUpperCase();
+
         lcd.clear();
         lcd.setCursor(0, 0);
-        lcd.print("Received Data:");
+        lcd.print("Received:");
         lcd.setCursor(0, 1);
-        lcd.print(data);
-        Serial.print("Received Data: ");
-        Serial.println(data);
-    }
-}
+        lcd.print(received);
 
-// LED ON / OFF
-char data = 0;
-void setup()
-{
-    Serial.begin(9600);
-    pinMode(13, OUTPUT);
-}
-void loop()
-{
-    if (Serial.available() > 0)
-    {
-        data = Serial.read();
-        Serial.print(data);
-        Serial.print("\ ");
-        if(data == '1')
-            digitalWrite(13, HIGH);
-        else if (data == '0')
-            digitalWrite(13, LOW);
+        Serial.print("Received Data: ");
+        Serial.println(received);
+
+        if (received == "LEDON")
+        {
+            digitalWrite(ledPin, HIGH);
+        }
+        else if (received == "LEDOFF")
+        {
+            digitalWrite(ledPin, LOW);
+        }
     }
 }
